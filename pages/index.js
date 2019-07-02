570c4181aa17719d7d9237937291320259a703cf
jasonfellows <jasonfellows@gmail.com>
2019-07-02 10:54:17 +0200

    Revert to boilerplate without ms_ui

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { RecipeCard } from 'marley-spoon-ui'
 
 const Title = styled.h1`
-  color: ${({ theme }) => theme.colors.pepper_black_35};
   font-family: "Comic Sans MS", cursive, sans-serif;
   font-size: 50px;
   text-align: center;
@@ -11,12 +9,9 @@ const Title = styled.h1`
 
 export default class Index extends Component {
   render () {
-    console.log(this.props)
-
     return (
       <>
         <Title>Hello World!</Title>
-        <RecipeCard image={{url: "asdf"}} />
       </>
     )
   }
--- ssr-server.js
+++ ssr-server.js
@@ -13,9 +13,9 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  server.listen(3006, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:3006')
   })
 })
 .catch((ex) => {
